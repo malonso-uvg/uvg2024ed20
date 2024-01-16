001eda364992b8b9783129ff8a2f6f5b7516be6e
Moises Alonso <maalonso@uvg.edu.gt>
2024-01-16 08:07:15 -0600

    Implementacion de Calculadora2

---
+++ EjemploHT1/src/Calculador2.java
@@ -0,0 +1,13 @@
+public class Calculador2 implements ICalculator{
+
+    @Override
+    public int add(int n1, int n2) {
+       return n1 + n2;
+    }
+
+    @Override
+    public int mult(int n1, int n2) {
+        return n1 * n2;
+    }
+    
+}
