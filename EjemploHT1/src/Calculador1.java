5787ce3e249f14b46540ea45c747bf423447bddf
Moises Alonso <maalonso@uvg.edu.gt>
2024-01-16 08:07:37 -0600

    Correccion de Calculadora 1

--- EjemploHT1/src/Calculador1.java
+++ EjemploHT1/src/Calculador1.java
@@ -6,9 +6,23 @@ public class Calculador1 implements ICalculator{
     public int mult(int n1, int n2){
 		int resultado = 0;
 
+        boolean n2IsNegative = n2 < 0;
+        boolean n1IsNegative = n1 < 0;
+
+
+        if (n2IsNegative){
+            n2 = -n2;
+        }
+
         // Utilizar un bucle para sumar 'n2' a 'resultado' 'n1' veces
         for (int i = 0; i < n2; i++) {
             resultado += n1;
+        }
+
+        if (n2IsNegative == n1IsNegative){
+            if (n2IsNegative){
+                resultado = -resultado;
+            }
         }
 
         return resultado;
