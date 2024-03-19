d6e2a1aaf65c85ee5d1960d05718d9b010fc45e4
Moises Alonso <maalonso@uvg.edu.gt>
2024-03-19 08:26:00 -0600

    Manejo de colisiones en repetidos

--- EjemploDesignPatterns/ProgramUI.java
+++ EjemploDesignPatterns/ProgramUI.java
@@ -1,4 +1,5 @@
 import java.util.AbstractMap;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ProgramUI{
@@ -7,12 +8,14 @@ public class ProgramUI{
         System.out.println("Seleccione el tipo de mapa");
         Scanner in = new Scanner(System.in);
         MapFactory<Integer, String> fabricaMap = new MapFactory<Integer, String>();
+        MapFactory<Integer, ArrayList<String>> fabricaMapRepetidos = new MapFactory<Integer, ArrayList<String>>();
 
         System.out.println("1. HashMap");
         System.out.println("2. Tree");
         System.out.println("3. LinkedHashMap");
-        AbstractMap<Integer, String> dictionary = fabricaMap.getMapInstance( Integer.parseInt(in.nextLine()) );
-
+        int option = Integer.parseInt(in.nextLine());
+        AbstractMap<Integer, String> dictionary = fabricaMap.getMapInstance( option );
+        AbstractMap<Integer, ArrayList<String>> repetidos = fabricaMapRepetidos.getMapInstance( option );
 
         System.out.println("Ingrese 'salir' en cualquier momento para terminar.");
 
@@ -38,11 +41,28 @@ public class ProgramUI{
             String nombre = in.nextLine();
 
             dictionary.put(id, nombre);
+
+            //Logica para los repetidos
+            if (repetidos.get(id) == null){ //La llave no existe
+                ArrayList<String> nuevoRepetido = new ArrayList<String>();
+                nuevoRepetido.add(nombre);
+                repetidos.put(id, nuevoRepetido);
+            } else {
+                repetidos.get(id).add(nombre);
+            }
         }
 
         System.out.println("Contenido del HashMap:");
         for (Integer id : dictionary.keySet()) {
             System.out.println("ID: " + id + ", Nombre: " + dictionary.get(id));
+        }
+
+        System.out.println("Contenido del HashMap de Repetidos:");
+        for (Integer id : repetidos.keySet()) {
+            for (String name : repetidos.get(id)){
+                System.out.println("ID: " + id + ", Nombre: " + name);
+            }
+            
         }
 
         in.close();
